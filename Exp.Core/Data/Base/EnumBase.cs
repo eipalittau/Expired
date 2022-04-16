57f76392a7825d95001fcefe97e4438985da0b1d
Patrik Eichmann <eipa@gmx.ch>
2022-04-16 12:46:46 +0200

    Update EnumBase.cs

--- Exp.Core/Data/Base/EnumBase.cs
+++ Exp.Core/Data/Base/EnumBase.cs
@@ -1,5 +1,9 @@
 namespace Exp.Data {
   public abstract class EnumBase {
-  
+    public int ID { get; init; }
+    public string Name { get; init; }
+    
+    protected EnumBase(int aID, string aName)
+      => (ID, Name) = (aID, aName);      
   }
 }
