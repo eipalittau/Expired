73044b5174a3acc8b74ead170518c8c5d45b9a70
Patrik Eichmann <eipa@gmx.ch>
2022-04-16 13:07:40 +0200

    Update EffectEnum.cs

--- Exp.Core/Data/Enumeration/EffectEnum.cs
+++ Exp.Core/Data/Enumeration/EffectEnum.cs
@@ -1,21 +1,25 @@
 namespace Exp.Data {
-  public sealed class EffectEnum : EnumBase {
-    #region Properties / Folder
-    private static int mCurrentID = 0;
-    internal static EffectEnum None = new(nameof(None));
-    public static EffectEnum Primary = new(nameof(Primary));
-    public static EffectEnum Secondary = new(nameof(Secondary));
-    public static EffectEnum Tertiary = new(nameof(Tertiary));
-    #endregion
+    public sealed class EffectEnum : EnumBase {
+        #region Properties / Folder
+        private static int mCurrentID = 0;
+        internal static EffectEnum None = new(nameof(None));
+        public static EffectEnum Primary = new(nameof(Primary));
+        public static EffectEnum Secondary = new(nameof(Secondary));
+        public static EffectEnum Tertiary = new(nameof(Tertiary));
+        #endregion
     
-    #region Konstruktor
-    private EffectEnum(string aName) : base(mCurrentID++, aName) { }
-    #endregion
+        #region Konstruktor
+        private EffectEnum(string aName) : base(mCurrentID++, aName) { }
+        #endregion
       
-    #region Methoden
-      public void Add(string aName) {
+        #region Methoden
+        public static IList<EffectEnum> Enumerate(DirectionEnum aDirection = DirectionEnum.None) {
+            return base.Enumerate<EffectEnum>(aDirection)
+        }
         
-      }
-    #endregion
-  }
+        public static void Add(string aName) {
+            
+        }
+        #endregion
+    }
 }
