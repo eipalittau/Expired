0fe17e1ee69313a4bfedf26d50a36c24bc60e564
Patrik Eichmann <eipa@gmx.ch>
2022-04-27 10:10:30 +0200

    Create DublicateItemException.cs

---
+++ Exp.Core/Exception/DublicateItemException.cs
@@ -0,0 +1,7 @@
+namespace Exp.Exception {
+    /// <summary>>Das Item mit der ID '{0}' existiert bereits.</summary>
+    public sealed class DublicateItemException : ExceptionBase {
+        public DublicateItemException(string aID)
+            : base("Exp.Exception.DublicateItemException", aID) { }
+    }
+}
