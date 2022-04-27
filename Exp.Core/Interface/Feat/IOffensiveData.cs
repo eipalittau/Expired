ec9ccc39167a0bf02969db5111c5d396ef2858c7
Patrik Eichmann <eipa@gmx.ch>
2022-04-27 09:35:46 +0200

    Update IOffensiveData.cs

--- Exp.Core/Interface/Feat/IOffensiveData.cs
+++ Exp.Core/Interface/Feat/IOffensiveData.cs
@@ -1,4 +1,7 @@
 ﻿namespace Exp.Data.Feat {
     public interface IOffensiveData : IFeatDataBase<IOffensiveData> {
+        public int OnAttack(params Exp.Interface.General.IDamageTypeData[] aDamageTypes);
+        public int OnDamage(params Exp.Interface.General.IDamageTypeData[] aDamageTypes);
+        public IDiceTypeData? OverrideDiceType(params Exp.Interface.General.IDamageTypeData[] aDamageTypes);
     }
 }
