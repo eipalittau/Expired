1077ee8c4d020e8e05c5c33d08a5ed7e02794eaa
Patrik Eichmann <eipa@gmx.ch>
2022-04-27 09:43:53 +0200

    Update BattleHardened.cs

--- Exp.DefaultMod/Data/Feat/Offensive/BattleHardened.cs
+++ Exp.DefaultMod/Data/Feat/Offensive/BattleHardened.cs
@@ -7,10 +7,10 @@ namespace Exp.DefaultMod.Feat.Offensive {
             : base(nameof(BattleHardened), 100, Api.General.Tier.Singleton.Get(nameof(Data.General.Tier.One)), null) {
             Name.Set(Util.LanguageEnum.Deutsch, "Kampferprobt");
             Name.Set(Util.LanguageEnum.English, "Battle hardened");
-            LoreDescription.Set(Util.LanguageEnum.Deutsch, "Habe viel auf die Fresse bekommen. +1 Angriff, +1 Schaden");
-            LoreDescription.Set(Util.LanguageEnum.English, "");
-            EffektDescription.Set(Util.LanguageEnum.Deutsch, "+1 Angriff, +1 Schaden");
-            EffektDescription.Set(Util.LanguageEnum.English, "+1 Angriff, +1 Schaden");
+            LoreDescription.Set(Util.LanguageEnum.Deutsch, "{\rtf1Immer Mitten in die Fresse rein...}");
+            LoreDescription.Set(Util.LanguageEnum.English, "{\rtf1Always right in the face...}");
+            EffektDescription.Set(Util.LanguageEnum.Deutsch, "{\rtf1+1 Angriff, +1 Schaden}");
+            EffektDescription.Set(Util.LanguageEnum.English, "{\rtf1+1 Angriff, +1 Schaden}");
         }
         #endregion
         
