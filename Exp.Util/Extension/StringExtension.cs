1f64e66490d7305b7d8ae69e52fa0319467410df
Patrik Eichmann <eipa@gmx.ch>
2022-04-27 09:53:38 +0200

    Create StringExtension.cs

---
+++ Exp.Util/Extension/StringExtension.cs
@@ -0,0 +1,7 @@
+namespace Exp.Extension {
+    public static class StringExtension {
+        public static bool HasData(this string[] aDatas) {
+            return (aDatas != null && aDamageTypes > 0);
+        }
+    }
+}
