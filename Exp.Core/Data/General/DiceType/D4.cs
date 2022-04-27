fe3201c79075bf6c9e355cf5ce784a8805117139
Patrik Eichmann <eipa@gmx.ch>
2022-04-27 09:28:27 +0200

    Create D4.cs

---
+++ Exp.Core/Data/General/DiceType/D4.cs
@@ -0,0 +1,15 @@
+using Exp.Data.General;
+
+namespace Exp.Data.General {
+    internal sealed class D4 : DiceTypeBase, IDiceTypeData {
+        #region Konstruktor
+        internal D4()
+            : base(nameof(D4), 100, 4) {
+            Name.Set(Util.LanguageEnum.Deutsch, "W4");
+            Name.Set(Util.LanguageEnum.English, "D4");
+            LoreDescription.Set(Util.LanguageEnum.Deutsch, "");
+            LoreDescription.Set(Util.LanguageEnum.English, "");
+        }
+        #endregion
+    }
+}
