4800bf7521e90e443b7a15541e23dc048512610e
Patrik Eichmann <eipa@gmx.ch>
2022-04-27 09:45:10 +0200

    Update Stonethrower.cs

--- Exp.DefaultMod/Data/Feat/Offensive/Stonethrower.cs
+++ Exp.DefaultMod/Data/Feat/Offensive/Stonethrower.cs
@@ -7,8 +7,8 @@ namespace Exp.DefaultMod.Feat.Offensive {
             : base(nameof(Stonethrower), 200, Api.General.Tier.Singleton.Get(nameof(Data.General.Tier.One)), Api.General.ActionType.Singleton.Get(nameof(Data.General.ActionType.Standard))) {
             Name.Set(Util.LanguageEnum.Deutsch, "Steinwerfer");
             Name.Set(Util.LanguageEnum.English, "Stone thrower");
-            LoreDescription.Set(Util.LanguageEnum.Deutsch, "");
-            LoreDescription.Set(Util.LanguageEnum.English, "");
+            LoreDescription.Set(Util.LanguageEnum.Deutsch, "{\rtf1Komm her und kämpfe wie ein richtiger Mann!}");
+            LoreDescription.Set(Util.LanguageEnum.English, "{\rtf1Come here and fight like a real man!}");
             EffektDescription.Set(Util.LanguageEnum.Deutsch, "{\rtf1Fernkampf: +1 Angriff, +1 Schaden\lineFernkampfangriff: 1W4 Schaden}");
             EffektDescription.Set(Util.LanguageEnum.English, "{\rtf1Fernkampf: +1 Angriff, +1 Schaden\lineFernkampfangriff: 1W4 Schaden}");        }
         #endregion
