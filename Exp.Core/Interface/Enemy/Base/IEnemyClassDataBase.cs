3f2154bd5e37b3cd51d0cb317b18bef4d61a57ca
Patrik Eichmann <eipa@gmx.ch>
2022-04-27 10:46:33 +0200

    Update IEnemyClassDataBase.cs

--- Exp.Core/Interface/Enemy/Base/IEnemyClassDataBase.cs
+++ Exp.Core/Interface/Enemy/Base/IEnemyClassDataBase.cs
@@ -1,5 +1,6 @@
 ﻿namespace Exp.Data.Enemy {
     public interface IEnemyClassDataBase : IDataBase {
+        /// <summary>Definiert die Reihenfolge im Kampf. Je höher dieser Wert ist, desto später kann der Charakter agieren.</summary>
         int Initiative { get; set; }
     }
 }
