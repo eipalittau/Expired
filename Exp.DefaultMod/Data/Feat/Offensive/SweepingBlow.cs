ece4cfba1014e182e713bf056cd38f405a853050
Patrik Eichmann <eipa@gmx.ch>
2022-04-27 19:59:52 +0200

    GetExtraAttack

--- Exp.DefaultMod/Data/Feat/Offensive/SweepingBlow.cs
+++ Exp.DefaultMod/Data/Feat/Offensive/SweepingBlow.cs
@@ -1,7 +1,12 @@
 ﻿using Exp.Data.Feat;
+using Exp.Data.General;
 
 namespace Exp.DefaultMod.Feat.Offensive {
     internal sealed class SweepingBlow : FeatDataBase<IOffensiveData>, IOffensiveData {
+        #region Properties / Felder
+        private bool DidHit { get; set; }
+        #endregion
+
         #region Konstruktor
         internal SweepingBlow()
             : base(nameof(SweepingBlow), 500, Api.General.Tier.Singleton.Get(nameof(Data.General.Tier.One)), Api.General.ActionType.Singleton.Get(nameof(Data.General.ActionType.Standard))) {
@@ -11,5 +16,27 @@ namespace Exp.DefaultMod.Feat.Offensive {
             LoreDescription.Set(Util.LanguageEnum.English, "");
         }
         #endregion
+
+        public int OnAttack(params IDamageTypeData[] aDamageTypes) {
+            DidHit = false;
+            return 0;
+        }
+
+        public int OnDamage(params IDamageTypeData[] aDamageTypes) {
+            DidHit = true;
+            return 0;
+        }
+
+        public IDiceTypeData? OverrideDiceType(params IDamageTypeData[] aDamageTypes) {
+            throw new NotImplementedException();
+        }
+
+        public int GetExtraAttack(params IDamageTypeData[] aDamageTypes) {
+            if (base.CheckDamageType(Api.General.DamageType.Singleton.Get(nameof(Data.General.DamageType.RangedCombat)), aDamageTypes)) {
+                return DidHit ? 1 : 0;
+            } else {
+                return 0;
+            }
+        }
     }
 }
