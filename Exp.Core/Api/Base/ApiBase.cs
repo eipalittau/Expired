e8f6ffa415d39ba126ae0b2bd59c9651f9fae8d5
Patrik Eichmann <eipa@gmx.ch>
2022-04-27 10:52:22 +0200

    Update ApiBase.cs

--- Exp.Core/Api/Base/ApiBase.cs
+++ Exp.Core/Api/Base/ApiBase.cs
@@ -31,14 +31,17 @@
                 return lItem;
             }
         }
-
+        
         private protected int Count() {
             return mDataList.Count;
         }
 
+        /// <summary>Fügt das übergebene Item der Sammlung hinzu.</summary>
+        /// <param name="aItem">Der neue Datensatz, welcher der Sammlung hinzugefügt werden soll.</param>
+        /// <exception cref="DublicateItemException">Falls die ID des Items bereits existiert, wird diese Exception geworfen.</exception>
         private protected void Add(T aItem) {
             if (mDataList.Any(x => x.ID.Equals(aItem.ID, StringComparison.InvariantCultureIgnoreCase))) {
-                // Patrik: Throw Dublicate Exception
+                throw new Exp.Exception.DublicateItemException(aItem.ID);
             }
 
             mDataList.Add(aItem);
