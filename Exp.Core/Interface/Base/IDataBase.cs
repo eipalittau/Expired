bb35af6b90df335e29aed65519e54acaab9260a9
Patrik Eichmann <eipa@gmx.ch>
2022-04-27 10:44:53 +0200

    Update IDataBase.cs

--- Exp.Core/Interface/Base/IDataBase.cs
+++ Exp.Core/Interface/Base/IDataBase.cs
@@ -1,8 +1,16 @@
 ﻿namespace Exp.Data {
     public interface IDataBase {
+        /// <summary>Eindeutige Identifikation des Datensatzes. Die ID kann zur bei der Erstellung gesetzt werden.</summary>
+        /// <exception cref="DublicateItemException">Falls die ID bereits existiert, wird diese Exception geworfen.</exception>
         string ID { get; init; }
+        
+        /// <summary>Der anzuzeigende Name in der jeweiligen Sprache. Für jede Sprache sollten Daten erfasst werden.</summary>
         Util.LanguageBasedData Name { get; }
+        
+        /// <summary>Die anzuzeigende Beschreibung in der jeweiligen Sprache. Für jede Sprache sollten Daten erfasst werden.</summary>
         Util.LanguageBasedData LoreDescription { get; }
+        
+        /// <summary>Ein nummerischer Wert für die Sortierung der Datensätze.</summary>
         int SortWeight { get; set; }
     }
 }
