cfa7af49cf77806039adcc03347b2ddc46f59dd1
Patrik Eichmann <eipa@gmx.ch>
2022-04-30 11:02:36 +0200

    Item

---
+++ Exp.DefaultMod/Data/Item/Item/ArmorFighter1.cs
@@ -0,0 +1,17 @@
+﻿using Exp.Data.Item;
+
+namespace Exp.DefaultMod.Item.ItemQuality {
+    internal sealed class ArmorFighter1 : ItemDataBase, IItemData {
+        #region Konstruktor
+        internal ArmorFighter1()
+            : base(nameof(ArmorFighter1), 1, 
+                  Api.Item.ItemType.Singleton.Get(nameof(Item.ItemType.Armor)), 
+                  Api.Enemy.EnemyClass.Singleton.Get(nameof(Enemy.Fighter)), false) {
+            Name.Set(Util.LanguageEnum.Deutsch, "Stoffrüstung des besiegten Kämpfers");
+            Name.Set(Util.LanguageEnum.English, "");
+            LoreDescription.Set(Util.LanguageEnum.Deutsch, "");
+            LoreDescription.Set(Util.LanguageEnum.English, "");
+        }
+        #endregion
+    }
+}
--- Exp.DefaultMod/Data/Item/Item/Base/ItemDataBase.cs
+++ Exp.DefaultMod/Data/Item/Item/Base/ItemDataBase.cs
@@ -1,51 +1,21 @@
 ﻿using Exp.Data.Item;
 
 namespace Exp.DefaultMod.Item.ItemQuality {
-    internal abstract class ItemDataBase : DataBase, IItemQualityDataBase {
+    internal abstract class ItemDataBase : DataBase, IItemDataBase {
         #region Properties / Felder
-        public IItemQualityDataBase? Downgrade { get; set; }
-        public IItemQualityDataBase? Upgrade { get; set; }
-        public bool CanBeDestroyed { get; set; }
-        public bool IsDefault { get; set; }
-        public IList<IEffectData> EffectList {
-            get {
-                return _EffectList.AsReadOnly();
-            }
-        }
+        public IItemTypeData ItemType { get; set; }
+        public Data.Enemy.IEnemyClassData? EnemyClass { get; set; }
+        public bool AlwaysAvailable { get; set; }
 
-        private List<IEffectData> _EffectList;
         #endregion
 
         #region Konstruktor
-        private protected ItemQualityDataBase(string aID, int aSortWeight, bool aCanBeDestroyed, bool aIsDefault, params float[] aEffectValues)
+        private protected ItemDataBase(string aID, int aSortWeight, IItemTypeData aItemType, Data.Enemy.IEnemyClassData? aEnemyClass, bool aAlwaysAvailable)
             : base(aID, aSortWeight) {
-            CanBeDestroyed = aCanBeDestroyed;
-            IsDefault = aIsDefault;
-            _EffectList = new List<IEffectData>() {
-                Api.Item.Effect.Singleton.Get(nameof(Effect.Primary)),
-                Api.Item.Effect.Singleton.Get(nameof(Effect.Secondary))
-            };
-
-            if (_EffectList.Count > aEffectValues.Length) {
-                Array.Resize(ref aEffectValues, EffectList.Count);
-            }
-
-            for (int lI = 0; lI < EffectList.Count; lI++) {
-                EffectList[lI].Value = aEffectValues[lI];
-            }
+            ItemType = aItemType;
+            EnemyClass=aEnemyClass;
+            AlwaysAvailable = aAlwaysAvailable;
         }
         #endregion
-    }
-}
-
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Exp.DefaultMod.Data.Item.Item.Base {
-    internal class ItemDataBase {
     }
 }
