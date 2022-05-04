9966beb98df02369ff7079434cc41a6be4295cd0
Patrik Eichmann <eipa@gmx.ch>
2022-05-04 16:39:45 +0200

    Update Cleric.cs

--- Exp.DefaultMod/Data/Enemy/EnemyClass/Cleric.cs
+++ Exp.DefaultMod/Data/Enemy/EnemyClass/Cleric.cs
@@ -1,12 +1,18 @@
 ﻿namespace Exp.DefaultMod.Enemy {
-    internal sealed class Cleric : Exp.Data.Enemy.EnemyClassBase, Exp.Data.Enemy.IEnemyClassData {
+    public sealed class Cleric : Exp.Data.Enemy.EnemyClassBase, Exp.Data.Enemy.IEnemyClassData {
         #region Konstruktor
-        internal Cleric()
+        private Cleric()
             : base(nameof(Cleric), 200, 200) {
             Name.Set(Util.LanguageEnum.Deutsch, "Kleriker");
             Name.Set(Util.LanguageEnum.English, "Cleric");
             LoreDescription.Set(Util.LanguageEnum.Deutsch, "");
             LoreDescription.Set(Util.LanguageEnum.English, "");
+        }
+        #endregion
+            
+        #region Methoden
+        public static void Add() {
+            new Cleric().AddInstance();
         }
         #endregion
     }
