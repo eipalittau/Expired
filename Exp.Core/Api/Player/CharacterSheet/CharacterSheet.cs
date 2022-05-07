3357246954aa126d9e03f0218481955acc9c1fb6
Patrik Eichmann <eipa@gmx.ch>
2022-05-07 11:02:40 +0200

    yxcv

--- Exp.Core/Api/Player/CharacterSheet/CharacterSheet.cs
+++ Exp.Core/Api/Player/CharacterSheet/CharacterSheet.cs
@@ -1,7 +1,6 @@
 ﻿namespace Exp.Api.Player {
     public sealed class CharacterSheet : Data.Player.ICharacterSheetData {
         #region Properties / Felder
-        public Util.LanguageEnum Language { get; private set; }
         public Data.Player.IPlayerClassData PlayerClass { get; init; }
         public Sheet.FeatData Feat { get; init; }
         public IList<Sheet.SkillData> SkillList { get; } = new List<Sheet.SkillData>();
@@ -11,9 +10,8 @@
         #endregion
 
         #region Konstruktor
-        private CharacterSheet(Data.Player.IPlayerClassData aPlayerClass, Util.LanguageEnum aLanguage) { 
+        private CharacterSheet(Data.Player.IPlayerClassData aPlayerClass) { 
             PlayerClass = aPlayerClass;
-            Language = aLanguage;
             Feat = new Sheet.FeatData();
             Skill.SkillGroup.Singleton.Enumerate().ToList().ForEach(x => SkillList.Add(new Sheet.SkillData(x)));
             Slot.Singleton.Enumerate().Where(x => x.Available).ToList().ForEach(x => EquipmentList.Add(new Sheet.EquipmentData(x)));
@@ -23,20 +21,16 @@
         #endregion
 
         #region Methoden
-        public static CharacterSheet Create(Util.LanguageEnum aLanguage) {
+        public static CharacterSheet Create() {
             Data.General.IDiceTypeData lDice = General.DiceType.Singleton.Get(Player.PlayerClass.Singleton.Count());
             
-            return Create(Player.PlayerClass.Singleton.Enumerate().ElementAt(lDice.Roll()), aLanguage);
+            return Create(Player.PlayerClass.Singleton.Enumerate().ElementAt(lDice.Roll()));
         }
 
-        public static CharacterSheet Create(Data.Player.IPlayerClassData aPlayerClass, Util.LanguageEnum aLanguage) {
-            CharacterSheet lNew = new(aPlayerClass, aLanguage);
+        public static CharacterSheet Create(Data.Player.IPlayerClassData aPlayerClass) {
+            CharacterSheet lNew = new(aPlayerClass);
 
             return lNew;
-        }
-
-        public void SetLanguage(Util.LanguageEnum aLanguage) {
-            Language = aLanguage;
         }
         #endregion
     }
