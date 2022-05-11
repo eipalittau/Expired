22c162c107161855f02acc5ef0775ec2b21ec8d4
Kakerlake <Kakerlake@DESKTOP-5CHJ43T>
2022-05-11 21:57:30 +0200

    review 11.5.22

---
+++ Exp.DefaultMod/Data/Player/PlayerClass/Aptitude/LoseAction.cs
@@ -0,0 +1,15 @@
+﻿namespace Exp.DefaultMod.Player.PlayerClass.Aptitude {
+    public sealed class LoseAction : Exp.Data.Misc.AptitudeDataBase, Exp.Data.Misc.IAptitudeData {
+        #region Konstruktor
+        public LoseAction()
+            : base(nameof(LoseAction), Api.General.CharacterChangerEnum.Feat) {
+            Name.Set(Util.LanguageEnum.Deutsch, "xx");
+            Name.Set(Util.LanguageEnum.English, "yy");
+            LoreDescription.Set(Util.LanguageEnum.Deutsch, "xxx");
+            LoreDescription.Set(Util.LanguageEnum.English, "yyy");
+            Modifier.Intervall = 1;
+            Modifier.Value = 2;
+        }
+        #endregion
+    }
+}
--- Exp.DefaultMod/Data/Player/PlayerClass/Banshee.cs
+++ Exp.DefaultMod/Data/Player/PlayerClass/Banshee.cs
@@ -1,8 +1,11 @@
 ﻿namespace Exp.DefaultMod.Player.PlayerClass {
     public sealed class Banshee : Exp.Data.Player.PlayerClassDataBase, Exp.Data.Player.IPlayerClassData {
         #region Konstruktor
+        /// <summary>
+        /// Mirko: create summary EVERYWHERE !!
+        /// </summary>
         private Banshee()
-            : base(nameof(Banshee), 500, "Mandy") {
+            : base(nameof(Banshee), 500, "Mandy", new Aptitude.LoseAction()) {
             Name.Set(Util.LanguageEnum.Deutsch, "Todesfee");
             Name.Set(Util.LanguageEnum.English, "Banshee");
             LoreDescription.Set(Util.LanguageEnum.Deutsch, "");
@@ -11,6 +14,9 @@
         #endregion
 
         #region Methoden
+        /// <summary>
+        /// blablabla
+        /// </summary>
         public static void Add() {
             AddInstance(new Banshee());
         }
